a6d816ffc6f8c3aa039b0d478ac800575f18e800
NebieAlert <mjnn100801@naver.com>
2023-10-27 09:44:04 +0900

    인풋 포커스 해결~.~

--- js/Header/header.js
+++ js/Header/header.js
@@ -32,13 +32,16 @@ form.addEventListener('submit', (e) => {
 
 // 브라우저 창에서 keydown 이벤트가 발생 시
 window.addEventListener('keydown', (e) => {
+
   // dataset 속성의 값이 지금 누른 키와 같은 요소를 찾고
   const slash = document.querySelector(`input[data-key = ${e.code}]`);
   // 요소가 없다면 아무 일도 일어나지 않음
   if (!slash) return;
+  // 활성 요소가 없으면 인풋으로 focus
+  if (Array.from(document.querySelectorAll('input')).every(el => el !== document.activeElement))
 
-  // 요소가 있다면 그 요소에 focus 하고
-  slash.focus();
+    // 요소가 있다면 그 요소에 focus 하고
+    slash.focus();
 
   // 찾은 요소에 transition이 시작 될 때
   onTransition(slash);
