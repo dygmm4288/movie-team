d20e2ccad31b3ddbcefdbc93353e15afeddccd90
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2023-10-30 09:41:23 +0900

    검색 안되던 버그 수정, 로드 시 index.js가 먼저 로드돼 다른 파일들 못 읽어 오던 내용 수정

--- js/page.js
+++ js/page.js
@@ -1,20 +1,26 @@
-import { router } from './index.js';
-import { movieDetail } from './moviedetail.js';
-import { routing } from './router.js';
-import { select } from './util.js';
+import { router } from "./index.js";
+import { movieDetail } from "./moviedetail.js";
+import { routing } from "./router.js";
+import { select } from "./util.js";
 
-const main = select('#main');
-const detailPage = document.querySelector('#detail-page');
-const filterButton = document.querySelector('.filter-button');
+// const main = select("#main");
+// const detailPage = document.querySelector("#detail-page");
+// const filterButton = document.querySelector(".filter-button");
 export function renderHome() {
-  detailPage.style.display = 'none';
-  filterButton.style.display = '';
-  main.style.display = '';
+  const main = select("#main");
+  const detailPage = document.querySelector("#detail-page");
+  const filterButton = document.querySelector(".filter-button");
+  detailPage.style.display = "none";
+  filterButton.style.display = "";
+  main.style.display = "";
 }
 export function renderMovieDetail(movie) {
-  main.style.display = 'none';
-  filterButton.style.display = 'none';
-  detailPage.style.display = '';
+  const main = select("#main");
+  const detailPage = document.querySelector("#detail-page");
+  const filterButton = document.querySelector(".filter-button");
+  main.style.display = "none";
+  filterButton.style.display = "none";
+  detailPage.style.display = "";
   return movieDetail(movie);
 }
 /* router */
@@ -23,7 +29,7 @@ export function handleLocation(link) {
   return (e) => {
     e.preventDefault();
     console.log(link);
-    window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
+    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
     history.pushState(link, null, window.location.href + link);
     routing(router);
   };
